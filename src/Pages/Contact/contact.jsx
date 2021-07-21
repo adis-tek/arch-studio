cb6bc982c499cd057f0b8d0da848061b46b08979
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-07-20 22:33:53 -0500

    form adjustments

--- src/Pages/Contact/contact.jsx
+++ src/Pages/Contact/contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './contact.scss';
 import PageLine from '../../Assets/icons/page-line.png';
 import Hero from '../../Assets/contact/desktop/image-hero.jpg';
@@ -7,7 +7,103 @@ import BlackArrow from '../../Assets/icons/icon-arrow-black.svg';
 import Arrow from '../../Assets/icons/icon-arrow.svg';
 import MapImage from '../../Assets/contact/desktop/image-map.png';
 
-function contact() {
+function Contact() {
+    const [errorTextName, setErrorTextName] = useState("error-text-initialized");
+    const [errorTextEmail, setErrorTextEmail] = useState("error-text-initialized");
+    const [errorTextPhone, setErrorTextPhone] = useState("error-text-initialized");
+    const [errorTextMessage, setErrorTextMessage] = useState("error-text-initialized");
+    {/* */}
+    const [inputName, setInputName] = useState("input-initialized");
+    const [inputEmail, setInputEmail] = useState("input-initialized");
+    const [inputPhone, setInputPhone] = useState("input-initialized");
+    const [inputMessage, setInputMessage] = useState("input-initialized");
+    {/* */}
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [text, setText] = useState("");
+    {/* */}
+    const [formSuccess, setFormSuccess] = useState("");
+
+    const toggleNameError = () => {
+        setErrorTextName("error-text");
+        setInputName("input-error");
+    }
+
+    const toggleEmailError = () => {
+        setErrorTextEmail("error-text");
+        setInputEmail("input-error");
+    }
+
+    const togglePhoneError = () => {
+        setErrorTextPhone("error-text");
+        setInputPhone("input-error");
+    }
+
+    const toggleMessageError = () => {
+        setErrorTextMessage("error-text-message");
+        setInputMessage("input-error");
+    }
+
+    const isEmail = (email) => {
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    }
+
+
+    const checkInputs = (e) => { 
+        let formChecks = 0;
+
+
+        if (name === "") {
+            console.log("Name Error");
+        } else if (name.trim().length < 3){
+            console.log("name is too short")
+            setErrorTextName("name-format-error");
+            setInputName("name-format-error-sign");
+        } else {
+            formChecks++;
+        }
+        if (email === "") {
+            console.log("Email Error");
+        } else if (isEmail(email) === false) {
+            console.log("invalid email format")
+            setErrorTextEmail("email-format-error");
+            setInputEmail("input-error");
+        } else {
+            formChecks++;
+        }
+        if (phone === "") {
+            console.log("Phone Error");
+        } else if (phone.trim().length < 10){
+            console.log("phone number is invalid")
+            setErrorTextPhone("phone-format-error");
+            setInputPhone("input-error");
+        } else {
+            formChecks++;
+        }
+        if (text === "") {
+            console.log("Text Error");
+        } else {
+            formChecks++;
+        }
+
+        if (formChecks === 4) {
+            console.log("FORM SUBMITTED SUCCESSFULLY",
+            {
+                Name: {name},
+                Email: {email},
+                Phone: {phone},
+                Text: {text},
+            })
+            setFormSuccess("Your message was sent successfully! We'll get back to you soon.")
+            return true;
+        }
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const isValid = checkInputs();
+    }
     return (
     <>
     <div className="page-header-faded">
@@ -86,13 +182,16 @@ function contact() {
                         <h1 className="contact-form-header">Connect<br></br>With Us</h1>
                     </div>
             </div>
-            <form className="form">
+            <form className="form" onSubmit={onSubmit}>
                 <label className="name-label"></label><br></br>
-                <input className="name-input" type="text" name="name" placeholder="Name"></input><br></br>
+                <input className="name-input" type="text" name="name" placeholder="Name" required onClick={toggleNameError}></input><br></br>
+                <span class={errorTextName}></span>
                 <label className="email-label"></label><br></br>
-                <input className="email-input" type="text" name="email" placeholder="Email"></input><br></br>
+                <input className={inputEmail} type="text" name="email" placeholder="Email" required onClick={toggleEmailError}></input><br></br>
+                <span class={errorTextEmail}></span>
                 <label className="message-label"></label><br></br>
-                <input className="message-input" type="text" name="text" placeholder="Message"></input><br></br>
+                <input className={inputMessage} type="text" name="text" required onClick={toggleMessageError} placeholder="Message"></input><br></br>
+                <span class={errorTextMessage}></span>
                 <div className="form-button-container">
                 <button className="button">
                     <img className="button-arrow" src={Arrow} />
@@ -118,4 +217,4 @@ function contact() {
 //   Message
 
 
-export default contact;
+export default Contact;
